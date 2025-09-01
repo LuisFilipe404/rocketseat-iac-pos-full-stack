24e8fcfe63391eb3d1bb1b5305c9b0273781512d
LuisFilipe404 <luisfilipesilvavasconcelos4@gmail.com>
2025-09-01 14:56:05 -0300

    .

--- index.ts
+++ index.ts
@@ -3,8 +3,15 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("primeiro-bucket", {
+const firstBucket = new aws.s3.Bucket("primeiro-bucket", {
   bucket: "primeiro-bucket-rocketseat-iac-1234567890",
+  tags: {
+    IAC: "true",
+  },
+});
+
+const secondBucket = new aws.s3.Bucket("segundo-bucket", {
+  bucket: "segundo-bucket-rocketseat-iac-1234567890",
   tags: {
     IAC: "true",
   },
@@ -19,9 +26,13 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
 });
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
-export const bucketInfo = bucket.bucket;
-export const bucketArn = bucket.arn;
+export const firstBucketName = firstBucket.id;
+export const firstBucketInfo = firstBucket.bucket;
+export const firstBucketArn = firstBucket.arn;
+
+// export const secondBucketName = secondBucket.id;
+// export const secondBucketInfo = secondBucket.bucket;
+// export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
